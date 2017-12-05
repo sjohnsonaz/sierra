61323634f2542b568cca3a40208cd3239914c580
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-12-05 00:37:21 -0700

    Added ServerTest.

---
+++ src/tests/ServerTests.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+
+import * as http from 'http';
+import RequestHandler from '../scripts/server/RequestHandler';
+
+let handler = new RequestHandler();
+
+handler.use(async function (context, result) {
+    //throw 'test error';
+    return context.send({ value: 'test' });
+});
+
+handler.error = async function (context, error) {
+    return error;
+};
+
+handler.view = async function (context, data) {
+    return '\
+        <!DOCTYPE html>\
+        <html>\
+        <head>\
+        <title>Sierra</title>\
+        </head>\
+        <body>\
+        <h1>Sierra</h1>\
+        <p>\
+        ' + JSON.stringify(data) + '\
+        </p>\
+        </body>\
+        </html>\
+    ';
+}
+
+let server = http.createServer(handler.create());
+
+let port = 3001;
+server.listen(port, () => {
+    console.log('Listening to port: ' + port);
+});
