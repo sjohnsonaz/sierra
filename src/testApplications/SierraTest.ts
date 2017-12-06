ca109fe866c6582b852ba7b46c514027e75ebfc9
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-12-06 14:42:20 -0700

    Created HandlebarsView middleware.

--- src/testApplications/SierraTest.ts
+++ src/testApplications/SierraTest.ts
@@ -1,8 +1,7 @@
 import Sierra, { Controller, middleware, route, Context, BodyParser, Session, method, view, json } from '../scripts/Sierra';
 
 import { request } from 'http';
-import * as fs from 'fs';
-import Handlebars from 'handlebars';
+import HandlebarsView from './HandlebarsView';
 
 class TestController extends Controller {
     constructor() {
@@ -43,22 +42,8 @@ class TestController extends Controller {
 
 let port = 3001;
 let testApplication = new Sierra();
-testApplication.view(async function (context, data, template) {
-    let templateFile = './src/testApplications/views/' + template + '.handlebars';
-    var templateText = await new Promise((resolve, reject) => {
-        fs.readFile(templateFile, {
-            encoding: 'utf8'
-        }, (err, data: string) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-    var compiledTemplate = Handlebars.compile(templateText);
-    return compiledTemplate(data);
-});
+HandlebarsView.viewRoot = './src/testApplications/views/';
+testApplication.view(HandlebarsView.handle);
 testApplication.addMiddleware = function (requestHandler) {
     requestHandler.use(BodyParser.handle);
     requestHandler.use(Session.handle);
