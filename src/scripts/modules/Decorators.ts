1f6ec5a3463927a9e56e0ed3971e6c3ddbd4cc7a
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-12-11 12:52:53 -0700

    Changed method and route decorator to take an optional Array of Verbs.

--- src/scripts/modules/Decorators.ts
+++ src/scripts/modules/Decorators.ts
@@ -4,17 +4,29 @@ import { IMiddleware } from '../server/IMiddleware';
 import RouteBuilder from '../router/RouteBuilder';
 import Controller from '../router/Controller';
 
-export function route<U extends IMiddleware<any, any>>(verb?: VerbType, name?: string | RegExp, pipeArgs: boolean = false) {
+export function route<U extends IMiddleware<any, any>>(verb?: VerbType | VerbType[], name?: string | RegExp, pipeArgs: boolean = false) {
     return function (target: Controller, propertyKey: string, descriptor: TypedPropertyDescriptor<U>) {
         var routeBuilder = RouteBuilder.getRouteBuilder(target);
-        routeBuilder.addDefinition(propertyKey, verb as any, name, pipeArgs);
+        if (verb instanceof Array) {
+            verb.forEach(verb => {
+                routeBuilder.addDefinition(propertyKey, verb as any, name, pipeArgs);
+            });
+        } else {
+            routeBuilder.addDefinition(propertyKey, verb as any, name, pipeArgs);
+        }
     }
 }
 
-export function method<U extends Function>(verb?: VerbType, name?: string | RegExp) {
+export function method<U extends Function>(verb?: VerbType | VerbType[], name?: string | RegExp) {
     return function (target: Controller, propertyKey: string, descriptor: TypedPropertyDescriptor<U>) {
         var routeBuilder = RouteBuilder.getRouteBuilder(target);
-        routeBuilder.addDefinition(propertyKey, verb as any, name, true);
+        if (verb instanceof Array) {
+            verb.forEach(verb => {
+                routeBuilder.addDefinition(propertyKey, verb as any, name, true);
+            });
+        } else {
+            routeBuilder.addDefinition(propertyKey, verb as any, name, true);
+        }
     }
 }
 
