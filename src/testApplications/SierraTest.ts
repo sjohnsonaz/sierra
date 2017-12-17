84666af7e215068c9abcfceee0b63d8d8f252928
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-12-16 23:47:58 -0700

    Cleaned SierraTest.

--- src/testApplications/SierraTest.ts
+++ src/testApplications/SierraTest.ts
@@ -70,28 +70,34 @@ class SessionGateway implements ISessionGateway<any> {
     async getId(context: Context): Promise<string> {
         return Uuid.create();
     }
+
     async load(context: Context, id: string): Promise<any> {
         return {
             id: id
         };
     }
+
     async save(context: Context, id: string, data: any): Promise<boolean> {
         return true;
     }
+
     async destroy(context: Context, id: string): Promise<boolean> {
         return true;
     }
+
     async regenerate(context: Context, id: string): Promise<any> {
         return {
             id: id
         };
     }
-    async  reload(context: Context, id: string): Promise<any> {
+
+    async reload(context: Context, id: string): Promise<any> {
         return {
             id: id
         };
     }
-    async  touch(context: Context, id: string): Promise<boolean> {
+
+    async touch(context: Context, id: string): Promise<boolean> {
         return true;
     }
 }
