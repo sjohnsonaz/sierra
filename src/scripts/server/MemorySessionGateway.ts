1331a20740e894b2ba9269f252a6842ed59aa082
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-01-15 00:25:12 -0700

    Added partial implementation of MemorySessionGateway.

---
+++ src/scripts/server/MemorySessionGateway.ts
@@ -0,0 +1,45 @@
+import Context from './Context';
+
+import { ISessionGateway } from './ISessionGateway';
+import Uuid from '../utils/Uuid';
+export default class MemorySessionGateway<T> implements ISessionGateway<T> {
+    data: {
+        [index: string]: T;
+    } = {};
+
+    async getId(context: Context): Promise<string> {
+        return Uuid.create();
+    }
+
+    async load(context: Context, id: string): Promise<T> {
+        return this.data[id] || ({ _id: id } as any);
+    }
+
+    async save(context: Context, id: string, data: T): Promise<boolean> {
+        if (!id) {
+            id = Uuid.create();
+        }
+        if (!data['_id']) {
+            data['_id'] = id;
+        }
+        this.data[id] = data;
+        return true;
+    }
+
+    async destroy(context: Context, id: string): Promise<boolean> {
+        delete this.data[id];
+        return true;
+    }
+
+    async regenerate(context: Context, id: string): Promise<T> {
+        return this.data[id] || ({ _id: id } as any);
+    }
+
+    async reload(context: Context, id: string): Promise<T> {
+        return this.data[id] || ({ _id: id } as any);
+    }
+
+    async touch(context: Context, id: string): Promise<boolean> {
+        return true;
+    }
+}
