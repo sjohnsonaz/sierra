51d8e71d396912ed573d6255265337c55ef19187
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-04-20 10:24:17 -0700

    Fixed Controller.getName test.

--- src/middleware/route/Controller.getName.test.ts
+++ src/middleware/route/Controller.getName.test.ts
@@ -6,31 +6,17 @@ let expect = chai.expect;
 import { Controller } from '../../Sierra';
 
 describe('RouteUtil.getControllerName()', () => {
-    it('should return name for Controllers ending in Controller', () => {
+    it('should return name for Controllers ending in Service', () => {
         class TestService extends Controller {
         }
         let base = Controller.getName(new TestService());
         expect(base).to.equal('test');
     });
 
-    it('should return name for Controllers ending in controller', () => {
-        class Testservice extends Controller {
-        }
-        let base = Controller.getName(new Testservice());
-        expect(base).to.equal('test');
-    });
-
-    it('should return name for Controllers ending in Service', () => {
+    it('should return name for Controllers ending in Controller', () => {
         class TestController extends Controller {
         }
         let base = Controller.getName(new TestController());
-        expect(base).to.equal('test');
-    });
-
-    it('should return name for Controllers ending in service', () => {
-        class Testcontroller extends Controller {
-        }
-        let base = Controller.getName(new Testcontroller());
         expect(base).to.equal('test');
     });
 
@@ -38,13 +24,6 @@ describe('RouteUtil.getControllerName()', () => {
         class TestRouter extends Controller {
         }
         let base = Controller.getName(new TestRouter());
-        expect(base).to.equal('test');
-    });
-
-    it('should return name for Controllers ending in router', () => {
-        class Testrouter extends Controller {
-        }
-        let base = Controller.getName(new Testrouter());
         expect(base).to.equal('test');
     });
 });
