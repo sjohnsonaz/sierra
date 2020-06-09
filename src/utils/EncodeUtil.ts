a1098b72bdba3ac917cb4675e3abba9ca172bcfb
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-06-09 09:45:12 -0700

    Updated EncodeUtil.

--- src/utils/EncodeUtil.ts
+++ src/utils/EncodeUtil.ts
@@ -1,75 +1,83 @@
-export default class EncodeUtil {
-    static objectToUrlString(obj: Object) {
-        var values = [];
-        for (var name in obj) {
-            if (obj.hasOwnProperty(name)) {
-                var value = obj[name];
-                if (value instanceof Array) {
-                    for (var index = 0, length = value.length; index < length; index++) {
-                        values.push(name + '[]=' + encodeURIComponent(value[index]));
-                    }
-                } else if (value !== undefined) {
-                    values.push(name + '=' + encodeURIComponent(value));
+export function objectToUrlString(obj: Object) {
+    var values = [];
+    for (var name in obj) {
+        if (obj.hasOwnProperty(name)) {
+            var value = obj[name];
+            if (value instanceof Array) {
+                for (var index = 0, length = value.length; index < length; index++) {
+                    values.push(name + '[]=' + encodeURIComponent(value[index]));
                 }
+            } else if (value !== undefined) {
+                values.push(name + '=' + encodeURIComponent(value));
             }
         }
-        return values.join('&');
     }
+    return values.join('&');
+}
 
-    static urlStringToObject(url: string): {
+export function urlStringToObject(url: string): [string, {
+    [index: string]: string | string[];
+}] {
+    const index = url.indexOf('?');
+    const base = url.substring(0, index);
+    url = url.substring(index + 1);
+    let obj: {
         [index: string]: string | string[];
-    } {
-        if (url.startsWith('?')) {
-            url = url.substring(1);
+    } = {};
+    url.split('&').forEach(part => {
+        let pieces = part.split('=');
+        if (pieces.length !== 2) {
+            throw 'Cannot parse URI';
         }
-        let obj = {};
-        url.split('&').forEach(part => {
-            let pieces = part.split('=');
-            if (pieces.length !== 2) {
-                throw 'Cannot parse URI';
-            }
-            let name = pieces[0];
-            let value = pieces[1];
-            if (name) {
-                // If name ends with [], we have an array.
-                if (name.endsWith('[]')) {
-                    name = name.substr(0, name.length - 2);
-                    if (name) {
-                        if (!obj[name]) {
-                            obj[name] = [];
-                        }
-                    } else {
-                        return;
-                    }
-                }
-                value = decodeURIComponent(value);
-                let valueArray = value.split(',');
-                if (valueArray.length > 1) {
-                    // If name already exists, we have an array.
-                    if (obj[name]) {
-                        if (!(obj[name] instanceof Array)) {
-                            obj[name] = [obj[name]];
-                        }
-                        valueArray.forEach(value => {
-                            obj[name].push(value);
-                        });
-                    } else {
-                        obj[name] = valueArray;
+        let name = pieces[0];
+        let value = pieces[1];
+        if (name) {
+            // If name ends with [], we have an array.
+            if (name.endsWith('[]')) {
+                name = name.substr(0, name.length - 2);
+                if (name) {
+                    if (!obj[name]) {
+                        obj[name] = [];
                     }
                 } else {
-                    // If name already exists, we have an array.
-                    if (obj[name]) {
-                        if (!(obj[name] instanceof Array)) {
-                            obj[name] = [obj[name], value];
-                        } else {
-                            obj[name].push(value);
-                        }
-                    } else {
-                        obj[name] = value;
-                    }
+                    return;
                 }
             }
-        });
-        return obj;
-    }
+            value = decodeURIComponent(value);
+            let valueArray = value.split(',');
+            if (valueArray.length > 1) {
+                // If name already exists, we have an array.
+                let entry = obj[name];
+                if (entry) {
+                    if (!(entry instanceof Array)) {
+                        obj[name] = [entry];
+                    }
+                    valueArray.forEach(value => {
+                        (entry as string[]).push(value);
+                    });
+                } else {
+                    obj[name] = valueArray;
+                }
+            } else {
+                // If name already exists, we have an array.
+                let entry = obj[name];
+                if (entry) {
+                    if (!(entry instanceof Array)) {
+                        obj[name] = [entry, value];
+                    } else {
+                        (entry as string[]).push(value);
+                    }
+                } else {
+                    obj[name] = value;
+                }
+            }
+        }
+    });
+    return [base, obj];
+}
+
+export function updateUrlStringObject(url: string, obj: Object): string {
+    let [base, data] = urlStringToObject(url);
+    let merged = Object.assign({}, data, obj);
+    return base + '?' + objectToUrlString(merged);
 }
