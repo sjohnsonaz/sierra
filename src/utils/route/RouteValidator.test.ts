7be6018a8e05f84fd08c9f223d2de35fc0bb4759
Sean Johnson <sjohnson@sjohnsonaz.com>
2021-03-26 16:05:30 -0700

    Fixed Route Validator.

--- src/utils/route/RouteValidator.test.ts
+++ src/utils/route/RouteValidator.test.ts
@@ -1,13 +1,13 @@
 import { createRoute } from './Route';
-import { Validator, NumberValidator, StringValidator } from './RouteValidator';
+import { createValidator } from './RouteValidator';
 
-describe('RouteValidator', function () {
+describe('createValidator', function () {
   it('should parse results', function () {
-    const validator = new Validator({
-      id: new NumberValidator(),
-      name: new StringValidator(),
+    const validator = createValidator({
+      id: Number,
+      name: String,
     });
-    const result = validator.run('1', 'test');
+    const result = validator('1', 'test');
     expect(result).toStrictEqual({
       id: 1,
       name: 'test',
@@ -17,15 +17,40 @@ describe('RouteValidator', function () {
   it('should run inside Routes', function () {
     const route = createRoute(
       (id, name) => `base/${id}/${name}`,
-      new Validator({
-        id: new NumberValidator(),
-        name: new StringValidator(),
-      }).parse
+      createValidator({
+        id: Number,
+        name: String,
+      })
     );
     const result = route.match('base/1/name');
+    if (result) {
+      const { id, name } = result;
+    }
     expect(result).toStrictEqual({
       id: 1,
       name: 'name',
     });
+  });
+
+  it('should handle route trip', function () {
+    const route = createRoute(
+      (id, name) => `base/${id}/${name}`,
+      createValidator({
+        id: Number,
+        name: String,
+      })
+    );
+
+    const data = {
+      id: 1,
+      name: 'name',
+    };
+
+    const { id, name } = data;
+
+    const url = route.run(id, name);
+    const match = route.match(url);
+
+    expect(match).toStrictEqual(data);
   });
 });
--- src/utils/route/RouteValidator.ts
+++ src/utils/route/RouteValidator.ts
@@ -1,30 +1,6 @@
-abstract class RouteValidator<T extends string | number> {
-  type: 'string' | 'number';
-  constructor(type: 'string' | 'number') {
-    this.type = type;
-  }
-  abstract run(value: string): T;
-}
-
-export class NumberValidator extends RouteValidator<number> {
-  constructor() {
-    super('number');
-  }
-  run(value: string) {
-    return parseInt(value);
-  }
-}
-
-export class StringValidator extends RouteValidator<string> {
-  constructor() {
-    super('string');
-  }
-  run(value: string) {
-    return value;
-  }
-}
-
-export class Validator<T extends Record<string, RouteValidator<any>>> {
+export class Validator<
+  T extends Record<string, StringConstructor | NumberConstructor>
+> {
   validators: T;
 
   constructor(validators: T) {
@@ -35,7 +11,17 @@ export class Validator<T extends Record<string, RouteValidator<any>>> {
     const result: ValidationResult<T> = {} as any;
     return Object.keys(this.validators).reduce((result, key, index) => {
       const validator = this.validators[key];
-      result[key as keyof typeof result] = validator.run(args[index]);
+      let _validator: typeof validateString | typeof validateNumber;
+      switch (validator) {
+        case Number:
+          _validator = validateNumber;
+          break;
+        case String:
+        default:
+          _validator = validateString;
+          break;
+      }
+      result[key as keyof typeof result] = _validator(args[index]) as any;
       return result;
     }, result);
   }
@@ -43,6 +29,36 @@ export class Validator<T extends Record<string, RouteValidator<any>>> {
   parse = (...args: any) => this.run(...args);
 }
 
-type ValidationResult<T extends Record<string, RouteValidator<any>>> = {
-  [Property in keyof T]: string;
+function validateString(value: string | undefined) {
+  if (typeof value === 'string') {
+    return value;
+  } else {
+    throw new ValidationError('not a string');
+  }
+}
+
+function validateNumber(value: number | undefined) {
+  const result = Number(value);
+  if (!isNaN(result) && isFinite(result)) {
+    return result;
+  } else {
+    throw new ValidationError('not a number');
+  }
+}
+
+export class ValidationError extends Error {}
+
+type ValidationResult<
+  T extends Record<string, StringConstructor | NumberConstructor>
+> = {
+  [Property in keyof T]: T[Property] extends NumberConstructor
+    ? number
+    : string;
 };
+
+export function createValidator<
+  T extends Record<string, StringConstructor | NumberConstructor>
+>(validators: T) {
+  const validator = new Validator(validators);
+  return validator.parse;
+}
